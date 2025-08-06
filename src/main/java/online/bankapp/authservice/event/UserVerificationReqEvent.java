bd292434cc8614f86502b7c4b424d6c06c355f00
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-08-06 21:39:48 +0200

    feat(auth-service): add UUID to UserVerificationReqEvent for request tracking

--- src/main/java/online/bankapp/authservice/event/UserVerificationReqEvent.java
+++ src/main/java/online/bankapp/authservice/event/UserVerificationReqEvent.java
@@ -3,9 +3,10 @@ package online.bankapp.authservice.event;
 import online.bankapp.authservice.model.vo.EmailAddress;
 
 import java.time.Instant;
+import java.util.UUID;
 
-public record UserVerificationReqEvent(EmailAddress email, Instant timestamp) {
+public record UserVerificationReqEvent(EmailAddress email, Instant timestamp, UUID id) {
     public UserVerificationReqEvent(EmailAddress email) {
-        this(email, Instant.now());
+        this(email, Instant.now(), UUID.randomUUID());
     }
 }
