7fdbed4b0b17d415aea94fe1907f5869bf711994
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-08-06 10:31:13 +0200

    refactor(auth-service): restructure exception classes and add global handler

--- src/main/java/online/bankapp/authservice/exception/ConverterException.java
+++ src/main/java/online/bankapp/authservice/exception/ConverterException.java
@@ -1,9 +1,10 @@
 package online.bankapp.authservice.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import online.bankapp.authservice.exception.base.ErrorAuthServiceException;
 
 @Slf4j
-public class ConverterException extends AuthServiceErrorException {
+public class ConverterException extends ErrorAuthServiceException {
     public ConverterException(String msg, Exception e) {
         super(msg, e);
     }
--- src/main/java/online/bankapp/authservice/exception/InvalidEmailFormatException.java
+++ src/main/java/online/bankapp/authservice/exception/InvalidEmailFormatException.java
@@ -1,9 +1,10 @@
 package online.bankapp.authservice.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import online.bankapp.authservice.exception.base.WarnAuthServiceException;
 
 @Slf4j
-public class InvalidEmailFormatException extends AuthServiceWarnException {
+public class InvalidEmailFormatException extends WarnAuthServiceException {
     public InvalidEmailFormatException(String message) {
         super(message);
     }
